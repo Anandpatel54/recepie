eb5dc97f8ef299af0783e04417ab0f4e60115aa7
Anand patel <panand054@gmail.com>
2024-05-11 16:38:35 +0530

    Conntact part

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,14 @@
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="w-[70%] m-auto mt-[10%] p-10 bg-green-100 rounded-lg">
+      <h1 className="text-7xl mt-5 font-extrablod text-green-600 mb-[5%]">
+        DO YOU WANT TO REACH US ?
+      </h1>
+      <button className="rounded-md  text-md bg-green-600 text-white py-2 px-2 hover:bg-green-700 duratoin-200">
+        REACH KRLO FRAANDS &nbsp; &#8594;
+      </button>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
