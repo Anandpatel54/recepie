06d1625c1e6a1a07283d486a5b19933308b8e742
Anand patel <panand054@gmail.com>
2024-05-13 16:28:28 +0530

    other

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
       to={`/recipes/${id}`}
       className="w-[30%] text-center mb-[3%] mr-[3.3%] b p-5 shadow-[10px_10px_15px_0px_rgba(0,0,0,0.1)] hover:scale-[1.02] duration-200"
     >
-      <img className="w-[80%]" src={image} alt={title} />
+      <img className="w-[80%] rounded-xl" src={image} alt={title} />
       <h1 className="text-2xl font-bold">{title}</h1>
       <p>{description.slice(0, 100)}...</p>
       <div className="flex justify-between text-zinc-400 mt-5">
